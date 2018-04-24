87ce1264b11e23fe614d69c6043bf904b5245608
DevExpressExampleBot <fakeEmail>
2018-04-24 14:14:43 +0300

    Source auto update [skip ci]

--- VB/TestSite/Default.aspx
+++ VB/TestSite/Default.aspx
@@ -1,59 +1,61 @@
 ﻿<%@ Page Language="vb" AutoEventWireup="true"  CodeFile="Default.aspx.vb" Inherits="_Default" %>
 
-<%@ Register Assembly="DevExpress.Web.ASPxGridView.v9.1, Version=9.1.2.0, Culture=neutral, PublicKeyToken=b88d1754d700e49a"
-	Namespace="DevExpress.Web.ASPxGridView" TagPrefix="dxwgv" %>
-<%@ Register Assembly="DevExpress.Web.ASPxEditors.v9.1, Version=9.1.2.0, Culture=neutral, PublicKeyToken=b88d1754d700e49a"
-	Namespace="DevExpress.Web.ASPxEditors" TagPrefix="dxe" %>
+<%@ Register Assembly="DevExpress.Web.v13.1, Version=13.1.4.0, Culture=neutral, PublicKeyToken=b88d1754d700e49a"
+    Namespace="DevExpress.Web.ASPxGridView" TagPrefix="dxwgv" %>
+<%@ Register Assembly="DevExpress.Web.v13.1, Version=13.1.4.0, Culture=neutral, PublicKeyToken=b88d1754d700e49a"
+    Namespace="DevExpress.Web.ASPxEditors" TagPrefix="dxe" %>
 
 <!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
 
 <html xmlns="http://www.w3.org/1999/xhtml" >
 <head runat="server">
-	<title>Untitled Page</title>
+    <title>Untitled Page</title>
 </head>
 <body>
 <script type="text/javascript">
 function RefreshRow(vi){
-	var tbNum = eval('tbNum'+vi);
-	var num = tbNum.GetText();
-	var codes = grid.cpCodes;
-	var used = 0;
-	var cboxes = new Array(codes.length);
-	for(var i=0; i<codes.length; i++){
-		var cbx = eval('cbx_'+codes.charAt(i)+vi);
-		cboxes[i] = cbx;
-		if(cbx.GetValue()) used++;
-	}
-	for(var i=0; i<codes.length; i++){
-		var cbx = cboxes[i];
-		cbx.SetEnabled(cbx.GetValue()||(used<num));
-	}
+    var tbNum = eval('tbNum'+vi);
+    var num = tbNum.GetText();
+    var codes = grid.cpCodes;
+    var used = 0;
+    var cboxes = new Array(codes.length);
+    for(var i=0; i<codes.length; i++){
+        var cbx = eval('cbx_'+codes.charAt(i)+vi);
+        cboxes[i] = cbx;
+        if(cbx.GetValue()) used++;
+    }
+    for(var i=0; i<codes.length; i++){
+        var cbx = cboxes[i];
+        cbx.SetEnabled(cbx.GetValue()||(used<num));
+    }
 }
 </script>
-	<form id="form1" runat="server">
-	<div>
-		&nbsp; &nbsp; &nbsp;&nbsp;
-		<dxwgv:ASPxGridView ID="ASPxGridView1" runat="server" AutoGenerateColumns="False"
-			KeyFieldName="ID" Width="599px" ClientInstanceName="grid" OnCustomCallback="ASPxGridView1_CustomCallback">
-			<Columns>
-				<dxwgv:GridViewDataTextColumn FieldName="ID" ReadOnly="True" VisibleIndex="0">
-					<EditFormSettings Visible="False" />
-				</dxwgv:GridViewDataTextColumn>
-				<dxwgv:GridViewDataTextColumn FieldName="Num" VisibleIndex="1">
-					<DataItemTemplate>
-						<dxe:ASPxTextBox ID="txtBox" Width="100px" runat="server" Value='<%#Eval("Num")%>' ClientSideEvents-ValueChanged='<%#"function(s,e){RefreshRow(" & Container.VisibleIndex & ");}"%>' ClientInstanceName='<%#"tbNum" & Container.VisibleIndex.ToString()%>'></dxe:ASPxTextBox>
-					</DataItemTemplate>
-				</dxwgv:GridViewDataTextColumn>
-			</Columns>
-		</dxwgv:ASPxGridView>
-		&nbsp;&nbsp;&nbsp;
-		<dxe:ASPxButton ID="ASPxButton1" runat="server" AutoPostBack="False" Text="Post Modifications"
-			Width="217px">
-			<ClientSideEvents Click="function(s, e) {
+    <form id="form1" runat="server">
+    <div>
+        &nbsp; &nbsp; &nbsp;&nbsp;
+<!--region #Markup-->
+        <dxwgv:ASPxGridView ID="ASPxGridView1" runat="server" AutoGenerateColumns="False"
+            KeyFieldName="ID" Width="599px" ClientInstanceName="grid" OnCustomCallback="ASPxGridView1_CustomCallback">
+            <Columns>
+                <dxwgv:GridViewDataTextColumn FieldName="ID" ReadOnly="True" VisibleIndex="0">
+                    <EditFormSettings Visible="False" />
+                </dxwgv:GridViewDataTextColumn>
+                <dxwgv:GridViewDataTextColumn FieldName="Num" VisibleIndex="1">
+                    <DataItemTemplate>
+                        <dxe:ASPxTextBox ID="txtBox" Width="100px" runat="server" Value='<%#Eval("Num")%>' ClientSideEvents-ValueChanged='<%#"function(s,e){RefreshRow(" & Container.VisibleIndex & ");}"%>' ClientInstanceName='<%#"tbNum" & Container.VisibleIndex.ToString()%>'></dxe:ASPxTextBox>
+                    </DataItemTemplate>
+                </dxwgv:GridViewDataTextColumn>
+            </Columns>
+        </dxwgv:ASPxGridView>
+<!--endregion #Markup-->
+        &nbsp;&nbsp;&nbsp;
+        <dxe:ASPxButton ID="ASPxButton1" runat="server" AutoPostBack="False" Text="Post Modifications"
+            Width="217px">
+            <ClientSideEvents Click="function(s, e) {
 	grid.PerformCallback('post');
 }" />
-		</dxe:ASPxButton>
-	</div>
-	</form>
+        </dxe:ASPxButton>
+    </div>
+    </form>
 </body>
 </html>
--- VB/TestSite/Default.aspx.vb
+++ VB/TestSite/Default.aspx.vb
@@ -1,5 +1,4 @@
-Imports Microsoft.VisualBasic
-Imports System
+﻿Imports System
 Imports System.Data
 Imports System.Configuration
 Imports System.Web
@@ -15,125 +14,122 @@ Imports DevExpress.Web.ASPxEditors
 Imports System.Text
 
 Partial Public Class _Default
-	Inherits System.Web.UI.Page
-	Private list() As Record
+    Inherits System.Web.UI.Page
 
-	Protected Sub Page_Load(ByVal sender As Object, ByVal e As EventArgs)
-		list = New Record(ASPxGridView1.SettingsPager.PageSize - 1){}
-		For i As Integer = 0 To ASPxGridView1.SettingsPager.PageSize - 1
-			list(i).id = -1
-			Dim vi As Integer = ASPxGridView1.VisibleStartIndex + i
-			Dim txtBox1 As ASPxTextBox = CType(ASPxGridView1.FindRowCellTemplateControl(vi, TryCast(ASPxGridView1.Columns("Num"), GridViewDataColumn), "txtBox"), ASPxTextBox)
-			If txtBox1 Is Nothing Then
-				Continue For
-			End If
-			Try
-				Dim sb As New StringBuilder()
-				For j As Integer = 0 To Codes.Length - 1
-					Dim code As String = Codes.Substring(j, 1)
-					Dim col As GridViewDataColumn = TryCast(ASPxGridView1.Columns(code), GridViewDataColumn)
-					Dim cbx As ASPxComboBox = CType(ASPxGridView1.FindRowCellTemplateControl(vi, col, "cbx_" & code), ASPxComboBox)
-					If Convert.ToBoolean(cbx.Value) Then
-						sb.Append(code)
-					End If
-				Next j
-				list(i).id = Convert.ToInt32(ASPxGridView1.GetRowValues(vi, ASPxGridView1.KeyFieldName))
-				list(i).num = Convert.ToInt32(txtBox1.Text)
-				list(i).code = sb.ToString()
-			Catch
-			End Try
+    Private list() As Record
 
-		Next i
-		BindGridToData(ASPxGridView1)
-		ASPxGridView1.JSProperties("cpCodes") = Codes
-	End Sub
+    Protected Sub Page_Load(ByVal sender As Object, ByVal e As EventArgs)
+        list = New Record(ASPxGridView1.SettingsPager.PageSize - 1){}
+        For i As Integer = 0 To ASPxGridView1.SettingsPager.PageSize - 1
+            list(i).id = -1
+            Dim vi As Integer = ASPxGridView1.VisibleStartIndex + i
+            Dim txtBox1 As ASPxTextBox = CType(ASPxGridView1.FindRowCellTemplateControl(vi, TryCast(ASPxGridView1.Columns("Num"), GridViewDataColumn), "txtBox"), ASPxTextBox)
+            If txtBox1 Is Nothing Then
+                Continue For
+            End If
+            Try
+                Dim sb As New StringBuilder()
+                For j As Integer = 0 To Codes.Length - 1
+                    Dim code As String = Codes.Substring(j, 1)
+                    Dim col As GridViewDataColumn = TryCast(ASPxGridView1.Columns(code), GridViewDataColumn)
+                    Dim cbx As ASPxComboBox = CType(ASPxGridView1.FindRowCellTemplateControl(vi, col, "cbx_" & code), ASPxComboBox)
+                    If Convert.ToBoolean(cbx.Value) Then
+                        sb.Append(code)
+                    End If
+                Next j
+                list(i).id = Convert.ToInt32(ASPxGridView1.GetRowValues(vi, ASPxGridView1.KeyFieldName))
+                list(i).num = Convert.ToInt32(txtBox1.Text)
+                list(i).code = sb.ToString()
+            Catch
+            End Try
 
-	Private Sub BindGridToData(ByVal grid As ASPxGridView)
-		Dim dt As DataTable = TryCast(Session("DT"), DataTable)
-		If dt Is Nothing Then
-			dt = New DataTable()
-			Dim col As DataColumn = dt.Columns.Add("ID", GetType(Integer))
-			col.AutoIncrement = True
-			dt.Columns.Add("Num", GetType(Integer)).AllowDBNull= False
-			dt.Columns.Add("Code", GetType(String)).AllowDBNull = False
-			dt.PrimaryKey = New DataColumn() { col }
-			dt.Rows.Add(New Object() { Nothing, 2, "BC" })
-			dt.Rows.Add(New Object() { Nothing, 2, "AD" })
-			dt.Rows.Add(New Object() { Nothing, 0, "" })
-			dt.Rows.Add(New Object() { Nothing, 0, "" })
-			dt.Rows.Add(New Object() { Nothing, 0, "" })
-			Session("DT") = dt
-		End If
-		grid.DataSource = dt
-		grid.DataBind()
-	End Sub
+        Next i
+        BindGridToData(ASPxGridView1)
+        ASPxGridView1.JSProperties("cpCodes") = Codes
+    End Sub
 
-	Private Const Codes As String = "ABCD"
+    Private Sub BindGridToData(ByVal grid As ASPxGridView)
+        Dim dt As DataTable = TryCast(Session("DT"), DataTable)
+        If dt Is Nothing Then
+            dt = New DataTable()
+            Dim col As DataColumn = dt.Columns.Add("ID", GetType(Integer))
+            col.AutoIncrement = True
+            dt.Columns.Add("Num", GetType(Integer)).AllowDBNull= False
+            dt.Columns.Add("Code", GetType(String)).AllowDBNull = False
+            dt.PrimaryKey = New DataColumn() { col }
+            dt.Rows.Add(New Object() { Nothing, 2, "BC" })
+            dt.Rows.Add(New Object() { Nothing, 2, "AD" })
+            dt.Rows.Add(New Object() { Nothing, 0, "" })
+            dt.Rows.Add(New Object() { Nothing, 0, "" })
+            dt.Rows.Add(New Object() { Nothing, 0, "" })
+            Session("DT") = dt
+        End If
+        grid.DataSource = dt
+        grid.DataBind()
+    End Sub
 
-	Protected Overrides Sub OnInit(ByVal e As EventArgs)
-		MyBase.OnInit(e)
-		For i As Integer = 0 To Codes.Length - 1
-			Dim code As String = Codes.Substring(i,1)
-			Dim col As GridViewDataColumn = TryCast(ASPxGridView1.Columns(code), GridViewDataColumn)
-			If col Is Nothing Then
-				col = New GridViewDataColumn(code)
-				ASPxGridView1.Columns.Add(col)
-				col.DataItemTemplate = New ComboTemplate()
-			End If
-		Next i
-	End Sub
+    Private Const Codes As String = "ABCD"
 
-	Protected Sub ASPxGridView1_CustomCallback(ByVal sender As Object, ByVal e As ASPxGridViewCustomCallbackEventArgs)
-		If e.Parameters = "post" Then
-			Dim dt As DataTable = TryCast(Session("DT"), DataTable)
-			For i As Integer = 0 To list.Length - 1
-				Dim row As DataRow = dt.Rows.Find(list(i).id)
-				If row IsNot Nothing Then
-					row("Num") = list(i).num
-					row("Code") = list(i).code
-				End If
-			Next i
-			ASPxGridView1.DataBind()
-		End If
-	End Sub
+    Protected Overrides Sub OnInit(ByVal e As EventArgs)
+        MyBase.OnInit(e)
+        For i As Integer = 0 To Codes.Length - 1
+            Dim code As String = Codes.Substring(i,1)
+            Dim col As GridViewDataColumn = TryCast(ASPxGridView1.Columns(code), GridViewDataColumn)
+            If col Is Nothing Then
+                col = New GridViewDataColumn(code)
+                ASPxGridView1.Columns.Add(col)
+                col.DataItemTemplate = New ComboTemplate()
+            End If
+        Next i
+    End Sub
 
-	Private Class ComboTemplate
-		Implements ITemplate
-		Public Sub New()
+    Protected Sub ASPxGridView1_CustomCallback(ByVal sender As Object, ByVal e As ASPxGridViewCustomCallbackEventArgs)
+        If e.Parameters = "post" Then
+            Dim dt As DataTable = TryCast(Session("DT"), DataTable)
+            For i As Integer = 0 To list.Length - 1
+                Dim row As DataRow = dt.Rows.Find(list(i).id)
+                If row IsNot Nothing Then
+                    row("Num") = list(i).num
+                    row("Code") = list(i).code
+                End If
+            Next i
+            ASPxGridView1.DataBind()
+        End If
+    End Sub
 
-		End Sub
+    Private Class ComboTemplate
+        Implements ITemplate
 
-		#Region "ITemplate Members"
+        Public Sub New()
 
-		Public Sub InstantiateIn(ByVal container As Control) Implements ITemplate.InstantiateIn
-			Dim gcontainer As GridViewDataItemTemplateContainer = CType(container, GridViewDataItemTemplateContainer)
-			Dim cbx As New ASPxComboBox()
-			cbx.ID = "cbx_" & gcontainer.Column.FieldName
-			cbx.ValueType = GetType(Boolean)
-			cbx.Items.Add("Yes", True)
-			cbx.Items.Add("Blank", False)
-			Dim code As String = TryCast(DataBinder.Eval(gcontainer.DataItem, "Code"), String)
-			Dim value As Boolean
-			If String.IsNullOrEmpty(code) Then
-				value = False
-			Else
-				value = code.Contains(gcontainer.Column.FieldName)
-			End If
-			cbx.Value = value
-			cbx.Width = Unit.Pixel(80)
-			Dim num As Integer = Convert.ToInt32(DataBinder.Eval(gcontainer.DataItem, "Num"))
-			cbx.ClientEnabled = value OrElse code.Length < num
-			cbx.ClientSideEvents.SelectedIndexChanged = "function(s,e){RefreshRow(" & gcontainer.VisibleIndex & ");}"
-			cbx.ClientInstanceName = "cbx_" & gcontainer.Column.FieldName & gcontainer.VisibleIndex.ToString()
-			gcontainer.Controls.Add(cbx)
-		End Sub
+        End Sub
 
-		#End Region
-	End Class
+        #Region "#ITemplateMembers"
 
-	Private Structure Record
-		Public id As Integer
-		Public num As Integer
-		Public code As String
-	End Structure
+        Public Sub InstantiateIn(ByVal container As Control)
+            Dim gcontainer As GridViewDataItemTemplateContainer = CType(container, GridViewDataItemTemplateContainer)
+            Dim cbx As New ASPxComboBox()
+            cbx.ID = "cbx_" & gcontainer.Column.FieldName
+            cbx.ValueType = GetType(Boolean)
+            cbx.Items.Add("Yes", True)
+            cbx.Items.Add("Blank", False)
+            Dim code As String = TryCast(DataBinder.Eval(gcontainer.DataItem, "Code"), String)
+            Dim value As Boolean = If(String.IsNullOrEmpty(code), False, code.Contains(gcontainer.Column.FieldName))
+            cbx.Value = value
+            cbx.Width = Unit.Pixel(80)
+            Dim num As Integer = Convert.ToInt32(DataBinder.Eval(gcontainer.DataItem, "Num"))
+            cbx.ClientEnabled = value OrElse code.Length < num
+            cbx.ClientSideEvents.SelectedIndexChanged = "function(s,e){RefreshRow(" & gcontainer.VisibleIndex & ");}"
+            cbx.ClientInstanceName = "cbx_" & gcontainer.Column.FieldName + gcontainer.VisibleIndex.ToString()
+            gcontainer.Controls.Add(cbx)
+        End Sub
+
+        #End Region ' #ITemplateMembers
+    End Class
+
+    Private Structure Record
+        Public id As Integer
+        Public num As Integer
+        Public code As String
+    End Structure
 End Class
